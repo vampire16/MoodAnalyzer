db58059bb2dc4955d167406040778d93b155caa3
vampire16 <siddheshsalvi525@gmai.com>
2020-03-06 17:38:32 +0530

    ADDED : 'I am in sad mood' should return Sad

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -2,5 +2,12 @@ public class MoodAnalyzer<analyseMood> {
     public static void main(String[] args) {
         System.out.println("welcome to mood analyzer");
     }
+
+    public String analyzer(String mood){
+        if (mood.contains("sad"))
+            return "Sad";
+        else
+            return "Happy";
+    }
 }
 
--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -3,4 +3,10 @@ import org.junit.Test;
 
 public class TestCases {
     MoodAnalyzer moodAnalyzer=new MoodAnalyzer();
+
+    @Test
+    public void givenMessage_whenMoodSad_shouldReturnSad(){
+        String mood=moodAnalyzer.analyzer("I am in sad mood");
+        Assert.assertEquals("Sad",mood);
+    }
 }
