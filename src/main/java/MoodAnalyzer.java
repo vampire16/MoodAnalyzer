37dddc4b88998cf5cd043c370949fa173bfb3b90
vampire16 <siddheshsalvi525@gmai.com>
2020-03-09 09:08:23 +0530

    MODIFIED : Ternary operator added

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,21 +1,14 @@
 public class MoodAnalyzer<analyseMood> {
     String message;
     public static void main(String[] args) {
-        System.out.println("welcome to mood analyzer");
+        System.out.println("Welcome to mood analyzer");
     }
 
-    public MoodAnalyzer() {
-    }
+    public MoodAnalyzer() { }
 
     public MoodAnalyzer(String message) {
         this.message=message;
     }
 
-    public String analyzer(){
-        if (message.contains("sad"))
-            return "Sad";
-        else
-            return "Happy";
-    }
+    public String analyzer(){ return (message.contains("sad"))? "Sad" : "Happy"; }
 }
-
--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -2,16 +2,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCases {
+    MoodAnalyzer moodAnalyzer;
 
     @Test
-    public void givenMessage_whenMoodSad_shouldReturnSad(){
-        MoodAnalyzer moodAnalyzer=new MoodAnalyzer("I am in sad mood");
+    public void givenMessage_WhenMoodSad_ShouldReturnSad(){
+        moodAnalyzer = new MoodAnalyzer("I am in sad mood");
         Assert.assertEquals("Sad",moodAnalyzer.analyzer());
     }
 
     @Test
-    public void givenMessage_whenMoodAny_shouldReturnHappy(){
-        MoodAnalyzer moodAnalyzer=new MoodAnalyzer("I am in any mood");
+    public void givenMessage_WhenMoodAny_ShouldReturnHappy(){
+        moodAnalyzer = new MoodAnalyzer("I am in any mood");
         Assert.assertEquals("Happy",moodAnalyzer.analyzer());
     }
 }
