ea7eb3337e2a17a9225c81a4b904738dfa68ba7d
vampire16 <siddheshsalvi525@gmai.com>
2020-03-11 10:22:50 +0530

    ADDED : New test method for custom exception

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -5,20 +5,31 @@ public class TestCases {
     MoodAnalyzer moodAnalyzer;
 
     @Test
-    public void givenMessage_WhenMoodSad_ShouldReturnSad(){
+    public void givenMessage_WhenMoodSad_ShouldReturnSad() throws MoodAnalysisException {
         moodAnalyzer = new MoodAnalyzer("I am in sad mood");
         Assert.assertEquals("Sad",moodAnalyzer.analyzer());
     }
 
     @Test
-    public void givenMessage_WhenMoodAny_ShouldReturnHappy(){
+    public void givenMessage_WhenMoodAny_ShouldReturnHappy() throws MoodAnalysisException {
         moodAnalyzer = new MoodAnalyzer("I am in any mood");
         Assert.assertEquals("Happy",moodAnalyzer.analyzer());
     }
 
     @Test
-    public void givenMessage_WhenNull_ThenShouldReturnHappy() {
+    public void givenMessage_WhenNull_ThenShouldReturnHappy() throws MoodAnalysisException {
         moodAnalyzer = new MoodAnalyzer(null);
         Assert.assertEquals("Happy",moodAnalyzer.analyzer());
+    }
+
+    @Test
+    public void givenMessage_WhenNull_ThenShouldReturnCustomException() {
+        try {
+            moodAnalyzer = new MoodAnalyzer(null);
+            moodAnalyzer.analyzer();
+        }
+        catch(MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.UserDefinedType.NULL_EXCEPTION,e.userDefinedType);
+        }
     }
 }
