4227938c0cf9727154107f47028efa5f7826702c
vampire16 <siddheshsalvi525@gmai.com>
2020-03-12 14:59:38 +0530

    REFACTORED : NoSuchMethodException catch block

--- src/main/java/MoodAnalyserFactory.java
+++ src/main/java/MoodAnalyserFactory.java
@@ -4,14 +4,14 @@ import java.lang.reflect.InvocationTargetException;
 public class MoodAnalyserFactory {
     public static MoodAnalyzer createMoodAnalyserObject() throws MoodAnalysisException{
         try {
-            Class<?> moodAnalyzerClass = Class.forName("MoodAnalyzergfd");
-            Constructor<?> moodConstructor = moodAnalyzerClass.getConstructor();
+            Class<?> moodAnalyzerClass = Class.forName("MoodAnalyzer");
+            Constructor<?> moodConstructor = moodAnalyzerClass.getConstructor(Integer.class);
             Object moodObject = moodConstructor.newInstance();
             return (MoodAnalyzer) moodObject;
         } catch (ClassNotFoundException e) {
             throw new MoodAnalysisException("class not found",MoodAnalysisException.UserDefinedType.NO_SUCH_CLASS);
         } catch (NoSuchMethodException e) {
-            e.printStackTrace();
+            throw new MoodAnalysisException("Method not found",MoodAnalysisException.UserDefinedType.NO_SUCH_METHOD);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
