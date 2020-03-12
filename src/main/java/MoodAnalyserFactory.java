b8a72040aacf69b7d5b806bfad4fcf8a855a1997
vampire16 <siddheshsalvi525@gmai.com>
2020-03-12 12:56:51 +0530

    ADDED : createMoodAnalyserObject method

--- src/main/java/MoodAnalyserFactory.java
+++ src/main/java/MoodAnalyserFactory.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class MoodAnalyserFactory {
+    public static MoodAnalyzer createMoodAnalyserObject(){
+        try {
+            Class<?> moodAnalyzerClass = Class.forName("MoodAnalyzer");
+            Constructor<?> moodConstructor = moodAnalyzerClass.getConstructor();
+            Object moodObject = moodConstructor.newInstance();
+            return (MoodAnalyzer) moodObject;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
