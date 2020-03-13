37382bfaede4b2b1de27f037a48b0dc29be39a85
vampire16 <siddheshsalvi525@gmai.com>
2020-03-13 10:58:48 +0530

    REFACTORED : new method created for constructor

--- src/main/java/MoodAnalyserFactory.java
+++ src/main/java/MoodAnalyserFactory.java
@@ -2,16 +2,9 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class MoodAnalyserFactory {
-    public static MoodAnalyzer createMoodAnalyserObject() throws MoodAnalysisException{
+    public static MoodAnalyzer createMoodAnalyserObject(Constructor constructor) throws MoodAnalysisException{
         try {
-            Class<?> moodAnalyzerClass = Class.forName("MoodAnalyzer");
-            Constructor<?> moodConstructor = moodAnalyzerClass.getConstructor(Integer.class);
-            Object moodObject = moodConstructor.newInstance();
-            return (MoodAnalyzer) moodObject;
-        } catch (ClassNotFoundException e) {
-            throw new MoodAnalysisException("class not found",MoodAnalysisException.UserDefinedType.NO_SUCH_CLASS);
-        } catch (NoSuchMethodException e) {
-            throw new MoodAnalysisException("Method not found",MoodAnalysisException.UserDefinedType.NO_SUCH_METHOD);
+            return (MoodAnalyzer) constructor.newInstance();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
@@ -20,5 +13,16 @@ public class MoodAnalyserFactory {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static Constructor getConstructor(String className,Class<?>... param) throws MoodAnalysisException {
+        try {
+            Class<?> moodClass = Class.forName(className);
+            return moodClass.getConstructor(param);
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalysisException("class not found",MoodAnalysisException.UserDefinedType.NO_SUCH_CLASS);
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalysisException("Method not found",MoodAnalysisException.UserDefinedType.NO_SUCH_METHOD);
+        }
     }
 }
